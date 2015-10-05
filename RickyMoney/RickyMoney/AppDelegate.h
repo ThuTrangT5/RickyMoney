a10eeebe1990c4ab1641c5612ff36db53b3cc704
AdelphaTech Inc <atahir@adelphatech.com>
2015-10-06 00:05:02 +0700

    - init

---
+++ RickyMoney/RickyMoney/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RickyMoney
+//
+//  Created by Adelphatech on 10/5/15.
+//  Copyright © 2015 thutrangt5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RickyMoney/RickyMoney/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RickyMoney
+//
+//  Created by Adelphatech on 10/5/15.
+//  Copyright © 2015 thutrangt5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
